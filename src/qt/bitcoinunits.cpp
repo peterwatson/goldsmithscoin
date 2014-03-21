92ae6d944be5dbda743ea2e5cb6cb75a202e419c
peterwatson <peterjameswatson@Peters-MacBook-Air.local>
2014-03-21 23:52:07 +0000

    Changed LTC ==> GSC
    In text editor changed LTC ==> GSC.  This give the coin a new
    abbreviation

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("GSC");
+    case mBTC: return QString("mGSC");
+    case uBTC: return QString::fromUtf8("μGSC");
     default: return QString("???");
     }
 }
